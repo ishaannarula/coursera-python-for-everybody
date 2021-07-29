df782e172997d75556b269901987863a3a81ec25
ishaan-narula <60092550+ishaan-narula@users.noreply.github.com>
2021-07-29 17:33:31 +0530

    Add files via upload

---
+++ 1 Coursera - Getting Started with Python - University of Michigan/Week 4/Exercise 2.3 on py4e Website.py
@@ -0,0 +1,4 @@
+hrs = input('Please enter the number of hours you work.')
+wage = input('Please enter your hourly rate.')
+salary = float(hrs) * float(wage)
+print('Based on the information provided, your total salary is',salary)
